5325db147bc63950426cab8da98296a980140176
Daniel Rodriguez <drodrigo95@hotmail.com>
2024-01-31 15:29:22 -0600

    setting up which teams will play each other

---
+++ client/features/tournament/Tournament.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Tournament = () => {
+
+    return (
+        <div>
+            <h1>Here goes the pool play</h1>
+        </div>
+    )
+}
+
+export default Tournament;
