0c0a10d33676976c3e55bbe606c15e8101e077a6
Daniel Rodriguez <drodrigo95@hotmail.com>
2024-04-24 14:45:29 -0500

    starting to config home page

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
-/**
- * COMPONENT
- */
-const Home = (props) => 
-{
+const Home = (props) => {
   const username = useSelector((state) => state.auth.me.username);
 
   return (
     <div>
       <h3>Welcome, {username}</h3>
+      <div>
+        <h1>2024 Florida Classic</h1>
+      </div>
     </div>
   );
 };
